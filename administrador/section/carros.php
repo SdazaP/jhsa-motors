8ff500503b62a39f930a2db7a1ff514d3b5246a9
SdazaP <creepersdp57@gmail.com>
2024-05-04 13:05:02 -0600

    registro para carros completo

--- administrador/section/carros.php
+++ administrador/section/carros.php
@@ -2,12 +2,12 @@
 <?php 
 /* print_r($_POST);
 print_r($_FILES) */
-$txtID=(isset($_POST['txtID']))?$_POST['txtID']:"";
 $txtMarca=(isset($_POST['txtMarca']))?$_POST['txtMarca']:"";
 $txtModelo=(isset($_POST['txtModelo']))?$_POST['txtModelo']:"";
 $txtAnio=(isset($_POST['txtAnio']))?$_POST['txtAnio']:"";
 $txtColor=(isset($_POST['txtColor']))?$_POST['txtColor']:"";
 $txtImagen=(isset($_FILES['txtImagen']['name']))?$_FILES['txtImagen']['name']:"";
+$txtPrecio=(isset($_POST['txtPrecio']))?$_POST['txtPrecio']:"";
 $action=(isset($_POST['action']))?$_POST['action']:"";
 
 /* echo $txtID."<br/>";
@@ -32,17 +32,26 @@ echo $action."<br/>"; */
 
 include("../config/bd.php");
 
+// Consulta SQL para obtener los colores
+$sql_col = "SELECT id, color FROM colores";
+$result_col = $conexion->query($sql_col);
+
+$sql_mar = "SELECT id, nombre FROM marca";
+$result_mar = $conexion->query($sql_mar);
+
 switch ($action) {
     case 'registrar':
 
         
-        $sentenciaSQL = $conexion->prepare("INSERT INTO `carros` (modelo, marca, anio, color, imagen) VALUES (:,'x' ,'Porche', '2018', 'Rojo', 'porche_911.jpg');");
-        $sentenciaSQL = $conexion->prepare("INSERT INTO `carros` (modelo, marca, anio, color, imagen) VALUES (:modelo, :marca, :anio, :color, :imagen)");
+        /* $sentenciaSQL = $conexion->prepare("INSERT INTO `carros` (`Id`, `modelo`, `marca`, `anio`, `color`, `imagen`, `precio`) VALUES (NULL, 'chevy', '2', '2018', '2', 'chevy.jpg', '500000')");
+        $sentenciaSQL->execute(); */
+        $sentenciaSQL = $conexion->prepare("INSERT INTO `carros` (modelo, marca, anio, color, imagen, precio) VALUES (:modelo, :marca, :anio, :color, :imagen, :precio)");
         $sentenciaSQL->bindParam(':modelo', $txtModelo);
         $sentenciaSQL->bindParam(':marca', $txtMarca);
         $sentenciaSQL->bindParam(':anio', $txtAnio);
         $sentenciaSQL->bindParam(':color', $txtColor);
         $sentenciaSQL->bindParam(':imagen', $txtImagen);
+        $sentenciaSQL->bindParam(':precio', $txtPrecio);
         $sentenciaSQL->execute();
 
         echo "presionado registrar";
@@ -73,13 +82,19 @@ switch ($action) {
 
             <div class ="form-group input-group mb-3">
                 <label class="input-group-text" for="inputGroupSelect01">ID</label>
-                <input type="text" class="form-control" name="txtID" id="tctID"  placeholder="ID">
+                <input type="text" class="form-control"  placeholder="ID automatico" disabled>
             </div>
 
             <div class="form-group input-group mb-3">
                 <label class="input-group-text" for="inputGroupSelect01">Marca</label>
                 <select class="form-select" name="txtMarca" id="marca">
                     <option selected>Selecciona</option>
+                    <?php
+                    // Iterar sobre los resultados y crear opciones del menú desplegable
+                    while ($row = $result_mar->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<option value='" . $row["id"] . "'>" . $row["nombre"] . "</option>";
+                    }
+                    ?>
                 </select>
             </div>
 
@@ -97,7 +112,18 @@ switch ($action) {
                 <label class="input-group-text" for="inputGroupSelect01">Color</label>
                 <select class="form-select" name="txtColor" id="color">
                     <option selected>Selecciona</option>
+                    <?php
+                    // Iterar sobre los resultados y crear opciones del menú desplegable
+                    while ($row = $result_col->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<option value='" . $row["id"] . "'>" . $row["color"] . "</option>";
+                    }
+                    ?>
                 </select>
+            </div>
+
+            <div class="form-group input-group mb-3">
+                <label class="input-group-text" for="inputGroupSelect01">Precio</label>
+                <input type="text" class="form-control" name="txtPrecio" id="precio" placeholder="$">
             </div>
 
             <div class="form-group">
